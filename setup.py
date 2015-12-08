f9ddba69af8b0974e8d0a0aa60a0a6419e1a14b8
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2015-12-08 21:18:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "hotdoc-tag-extension",
+    name = "hotdoc_tag_extension",
     version = "0.6",
     keywords = "tags metadata custom filtering hotdoc",
-    url='https://github.com/MathieuDuponchelle/hotdoc-c-extension',
+    url='https://github.com/hotdoc/hotdoc_tag_extension',
     author_email = 'mathieu.duponchelle@opencreed.com',
     license = 'LGPL',
     description = ("An extension for hotdoc that allows defining new valid tags,"
                     " and filtering the output based on symbol's tags"),
     author = "Mathieu Duponchelle",
     packages = find_packages(),
-    entry_points = {'hotdoc.extensions': 'get_extension_classes = tag_extension:get_extension_classes'},
+    entry_points = {'hotdoc.extensions': 'get_extension_classes = hotdoc_tag_extension.tag_extension:get_extension_classes'},
     install_requires = [
         'hotdoc>=0.6',
     ]
